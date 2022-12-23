2e3ea913b585bebf5771fccdceefb08ddeebc125
vuongpham <phamnhuvuong96@gmail.com>
2022-12-23 13:11:09 +0700

    first

--- src/pages/Companies.tsx
+++ src/pages/Companies.tsx
@@ -1,12 +1,11 @@
 import CompaniesList from 'components/Company/CompaniesList';
-import { warn } from 'console';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
-const acceptRole = ['superadmin1', 'superobsever'];
+
+const acceptRole = ['superadmin', 'superobsever'];
 
 const Companies: React.FC = (): JSX.Element => {
-  const currentRole = useSelector((state: any) => state.auth.user);
-  console.log(currentRole);
+  const currentRole = useSelector((state: any) => state.auth.user.role);
 
   if (!acceptRole.includes(currentRole)) {
     alert('You are not allow to access this page!');
